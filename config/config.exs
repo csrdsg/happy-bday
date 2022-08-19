f93f10996b2ae37961f8eb0dc47d1c8e6117b663
csrdsg <csrds.g@gmail.com>
2022-08-19 13:25:59 +0200

    ecto migration almost done

--- config/config.exs
+++ config/config.exs
@@ -2,13 +2,11 @@ import Config
 
 config :happy_bday, HappyBday.Repo,
   database: "happy_bday_repo",
-  username: "user",
-  password: "pass",
   hostname: "localhost"
 
 config :happy_bday, ecto_repos: [HappyBday.Repo]
 
 
-config :acme, Acme.Scheduler,
+config :happy_bday, Acme.Scheduler,
   jobs: [    {"@daily",         {HappyBday, :happybday, []}}
 ]
